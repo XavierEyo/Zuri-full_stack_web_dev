316807b5cd51a9f6c43c80a8bf83da4d1514ebf9
XavierEyo <udoeyo91@gmail.com>
2022-06-13 17:15:17 +0100

    tasks

--- python_tasks/Rock_paper_scissors.py
+++ python_tasks/Rock_paper_scissors.py
@@ -1,24 +1,25 @@
 import random
 
-player1_choice = input("choose one: Rock, Paper or Scissors?")
-choices = ["Rock", "Paper", "Scissors"]
+print(f"\nR is Rock\nP is paper\nS is scissors\n")
+player1_choice = input("choose one: R, P or S?")
+choices = ["R", "P", "S"]
 computer_choice = random.choice(choices)
 print(f"\nYou chose {player1_choice}, computer chose {computer_choice}.\n")
 
 if player_1_choice == computer_choice:
     print(f"Both players selected {player_1_choice}. It's a draw!")
-elif player1_choice == "Rock":
-    if computer_choice == "Scissors":
+elif player1_choice == "R":
+    if computer_choice == "S":
         print("Rock smashes scissors! You win!")
     else:
         print("Paper covers rock! You lose.")
-elif player1_choice == "Paper":
-    if computer_choice == "Rock":
+elif player1_choice == "P":
+    if computer_choice == "R":
         print("Paper covers rock! You win!")
     else:
         print("Scissors cuts paper! You lose.")
-elif player1_choice == "Scissors":
-    if computer_choice == "Paper":
+elif player1_choice == "S":
+    if computer_choice == "P":
         print("Scissors cuts paper! You win!")
     else:
         print("Rock smashes scissors! You lose.")
