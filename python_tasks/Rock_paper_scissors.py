679a4e70ac3b879ebad12a5dbf2771baf803418a
XavierEyo <udoeyo91@gmail.com>
2022-06-13 17:42:50 +0100

    tasks

--- python_tasks/Rock_paper_scissors.py
+++ python_tasks/Rock_paper_scissors.py
@@ -1,23 +1,23 @@
 import random
 
-player1 = input("Enter a choice (r, p, s): ")
+player= input("Enter a choice (r, p, s): ")
 options = ["r", "p", "s"]
 computer = random.choice(options)
 print(f"\nYou chose {player1}, computer chose {computer}.\n")
 
-if player1 == computer:
+if player == computer:
     print(f"Both players selected {player1}. It's a tie!")
-elif player1 == "r":
+elif player == "r":
     if computer == "s":
         print("Rock smashes scissors! You win!")
     else:
         print("Paper covers rock! You lose.")
-elif player1 == "p":
+elif player == "p":
     if computer == "r":
         print("Paper covers rock! You win!")
     else:
         print("Scissors cuts paper! You lose.")
-elif player1 == "s":
+elif player == "s":
     if computer == "p":
         print("Scissors cuts paper! You win!")
     else:
